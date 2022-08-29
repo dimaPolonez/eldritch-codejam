0ada7c52b36668363d5ffc56d9b2313c196704bc
dimaPolonez <dima-barxat@yandex.ru>
2022-08-29 16:43:54 +0300

    feat: add difficulty

--- js/algorithm.js
+++ js/algorithm.js
@@ -18,7 +18,17 @@ let greenArr = [];
 let brownArr = [];
 let blueArr = [];
 
+export let greenArrCon = [];
+export let brownArrCon = [];
+export let blueArrCon = [];
+
+let stageSum1 = null;
+let stageSum2 = null;
+let stageSum3 = null;
+
 export let fullStackArr = [];
+
+let activeDiff = null;
 
 
 export function ancientLogic(ancient) {
@@ -29,14 +39,15 @@ export function ancientLogic(ancient) {
             green1.textContent = value.firstStage.greenCards;
             blue1.textContent = value.firstStage.blueCards;
             brown1.textContent = value.firstStage.brownCards;
+            stageSum1 = value.firstStage.greenCards + value.firstStage.blueCards + value.firstStage.brownCards;
             green2.textContent = value.secondStage.greenCards;
             blue2.textContent = value.secondStage.blueCards;
             brown2.textContent = value.secondStage.brownCards;
+            stageSum2 = value.secondStage.greenCards + value.secondStage.blueCards + value.secondStage.brownCards; 
             green3.textContent = value.thirdStage.greenCards;
             blue3.textContent = value.thirdStage.blueCards;
             brown3.textContent = value.thirdStage.brownCards;
-            randomStack();
-            fullStack();
+            stageSum3 = value.thirdStage.greenCards + value.thirdStage.blueCards + value.thirdStage.brownCards;
         }
     })
 }
@@ -46,104 +57,297 @@ function randomStack() {
     let fullBrown = Number(brown1.textContent) + Number(brown2.textContent) + Number(brown3.textContent);
     let fullBlue = Number(blue1.textContent) + Number(blue2.textContent) + Number(blue3.textContent);
 
+    function getRandom(numberMax) {
+        return (Math.trunc(Math.random() * (numberMax - 1) + 1));
+    }
+
     for (let i = 0; i < fullGreen; i++) {
-        let num = Math.trunc(getRandom(greenCardsData.length));
-        if (greenArr.includes(num)) {
-            i -= 1;
-        } else {
-            greenArr.push(num);
+        let num = getRandom(greenCardsData.length);
+
+        if (activeDiff === 0) {
+
+            if (greenArr.includes(greenCardsData[num].cardFace)) {
+                i -= 1;
+            } else {
+                if (greenCardsData[num].difficulty === 'easy') {
+                    greenArr.push(greenCardsData[num].cardFace);
+                } else if ((greenCardsData[num].difficulty === 'normal') && ((greenArr.length > 3) && (greenArr.length <= fullGreen))) {
+                    greenArr.push(greenCardsData[num].cardFace);
+                } else {
+                    i -= 1;
+                }
+            }
+        }
+
+        if (activeDiff === 1) {
+
+            if (greenArr.includes(greenCardsData[num].cardFace)) {
+                i -= 1;
+            } else {
+                if (greenCardsData[num].difficulty !== 'hard') {
+                    greenArr.push(greenCardsData[num].cardFace);
+                } else {
+                    i -= 1;
+                }
+            }
+        }
+
+        if (activeDiff === 2) {
+
+            if (greenArr.includes(greenCardsData[num].cardFace)) {
+                i -= 1;
+            } else {
+                greenArr.push(greenCardsData[num].cardFace);
+            }
+        }
+
+        if (activeDiff === 3) {
+
+            if (greenArr.includes(greenCardsData[num].cardFace)) {
+                i -= 1;
+            } else {
+                if (greenCardsData[num].difficulty !== 'easy') {
+                    greenArr.push(greenCardsData[num].cardFace);
+                } else {
+                    i -= 1;
+                }
+            }
+        }
+
+        if (activeDiff === 4) {
+
+            if (greenArr.includes(greenCardsData[num].cardFace)) {
+                i -= 1;
+            } else {
+                if (greenCardsData[num].difficulty === 'hard') {
+                    greenArr.push(greenCardsData[num].cardFace);
+                } else if ((greenCardsData[num].difficulty === 'normal') && ((greenArr.length > 3) && (greenArr.length <= fullGreen))) {
+                    greenArr.push(greenCardsData[num].cardFace);
+                } else {
+                    i -= 1;
+                }
+            }
         }
     }
 
     for (let i = 0; i < fullBrown; i++) {
-        let num = Math.trunc(getRandom(brownCardsData.length));
-        if (brownArr.includes(num)) {
-            i -= 1;
-        } else {
-            brownArr.push(num);
+        let num = getRandom(brownCardsData.length);
+
+        if (activeDiff === 0) {
+
+            if (brownArr.includes(brownCardsData[num].cardFace)) {
+                i -= 1;
+            } else {
+                if (brownCardsData[num].difficulty === 'easy') {
+                    brownArr.push(brownCardsData[num].cardFace);
+                } else if ((brownCardsData[num].difficulty === 'normal') && ((brownArr.length > 4) && (brownArr.length <= fullBrown))) {
+                    brownArr.push(brownCardsData[num].cardFace);
+                } else {
+                    i -= 1;
+                }
+            }
+        }
+
+        if (activeDiff === 1) {
+
+            if (brownArr.includes(brownCardsData[num].cardFace)) {
+                i -= 1;
+            } else {
+                if (brownCardsData[num].difficulty !== 'hard') {
+                    brownArr.push(brownCardsData[num].cardFace);
+                } else {
+                    i -= 1;
+                }
+            }
+        }
+
+        if (activeDiff === 2) {
+
+            if (brownArr.includes(brownCardsData[num].cardFace)) {
+                i -= 1;
+            } else {
+                brownArr.push(brownCardsData[num].cardFace);
+            }
+        }
+
+        if (activeDiff === 3) {
+
+            if (brownArr.includes(brownCardsData[num].cardFace)) {
+                i -= 1;
+            } else {
+                if (brownCardsData[num].difficulty !== 'easy') {
+                    brownArr.push(brownCardsData[num].cardFace);
+                } else {
+                    i -= 1;
+                }
+            }
+        }
+
+        if (activeDiff === 4) {
+
+            if (brownArr.includes(brownCardsData[num].cardFace)) {
+                i -= 1;
+            } else {
+                if (brownCardsData[num].difficulty === 'hard') {
+                    brownArr.push(brownCardsData[num].cardFace);
+                } else if ((brownCardsData[num].difficulty === 'normal') && ((brownArr.length > 4) && (brownArr.length <= fullBrown))) {
+                    brownArr.push(brownCardsData[num].cardFace);
+                } else {
+                    i -= 1;
+                }
+            }
         }
     }
 
     for (let i = 0; i < fullBlue; i++) {
-        let num = Math.trunc(getRandom(blueCardsData.length));
-        if (blueArr.includes(num)) {
-            i -= 1;
-        } else {
-            blueArr.push(num);
+        let num = getRandom(blueCardsData.length);
+
+        if (activeDiff === 0) {
+
+            if (blueArr.includes(blueCardsData[num].cardFace)) {
+                i -= 1;
+            } else {
+                if (blueCardsData[num].difficulty === 'easy') {
+                    blueArr.push(blueCardsData[num].cardFace);
+                } else if ((blueCardsData[num].difficulty === 'normal') && ((blueArr.length > 3) && (blueArr.length <= fullBlue))) {
+                    blueArr.push(blueCardsData[num].cardFace);
+                } else {
+                    i -= 1;
+                }
+            }
+        }
+
+        if (activeDiff === 1) {
+
+            if (blueArr.includes(blueCardsData[num].cardFace)) {
+                i -= 1;
+            } else {
+                if (blueCardsData[num].difficulty !== 'hard') {
+                    blueArr.push(blueCardsData[num].cardFace);
+                } else {
+                    i -= 1;
+                }
+            }
+        }
+
+        if (activeDiff === 2) {
+
+            if (blueArr.includes(blueCardsData[num].cardFace)) {
+                i -= 1;
+            } else {
+                blueArr.push(blueCardsData[num].cardFace);
+            }
+        }
+
+        if (activeDiff === 3) {
+
+            if (blueArr.includes(blueCardsData[num].cardFace)) {
+                i -= 1;
+            } else {
+                if (blueCardsData[num].difficulty !== 'easy') {
+                    blueArr.push(blueCardsData[num].cardFace);
+                } else {
+                    i -= 1;
+                }
+            }
+        }
+
+        if (activeDiff === 4) {
+
+            if (blueArr.includes(blueCardsData[num].cardFace)) {
+                i -= 1;
+            } else {
+                if (blueCardsData[num].difficulty === 'hard') {
+                    blueArr.push(blueCardsData[num].cardFace);
+                } else if ((blueCardsData[num].difficulty === 'normal') && ((blueArr.length > 3) && (blueArr.length <= fullBlue))) {
+                    blueArr.push(blueCardsData[num].cardFace);
+                } else {
+                    i -= 1;
+                }
+            }
         }
     }
 
-    function getRandom(numberMax) {
-        return Math.random() * (numberMax - 1) + 1;
-    }
+    greenArrCon = greenArr.slice();
+    brownArrCon = brownArr.slice();
+    blueArrCon = blueArr.slice();
 }
 
 export function fullStack() {
 
     if (Number(blue3.textContent) > 0) {
         for (let i = 0; i < Number(blue3.textContent); i++) {
-            fullStackArr.push(`url(./assets/MythicCards/blue/blue${blueArr.pop(blueArr[i])}.png)`);
+            fullStackArr.push(blueArr.pop(blueArr[i]));
         }
+
     }
 
     if (Number(brown3.textContent) > 0) {
         for (let i = 0; i < Number(brown3.textContent); i++) {
-            fullStackArr.push(`url(./assets/MythicCards/brown/brown${brownArr.pop(brownArr[i])}.png)`);
+            fullStackArr.push(brownArr.pop(brownArr[i]));         
         }
     }
 
 
     if (Number(green3.textContent) > 0) {
         for (let i = 0; i < Number(green3.textContent); i++) {
-            fullStackArr.push(`url(./assets/MythicCards/green/green${greenArr.pop(greenArr[i])}.png)`);
+            fullStackArr.push(greenArr.pop(greenArr[i]));
         }
     }
 
     if (Number(blue2.textContent) > 0) {
         for (let i = 0; i < Number(blue2.textContent); i++) {
-            fullStackArr.push(`url(./assets/MythicCards/blue/blue${blueArr.pop(blueArr[i])}.png)`);
+            fullStackArr.push(blueArr.pop(blueArr[i]));
         }
     }
 
     if (Number(brown2.textContent) > 0) {
         for (let i = 0; i < Number(brown2.textContent); i++) {
-            fullStackArr.push(`url(./assets/MythicCards/brown/brown${brownArr.pop(brownArr[i])}.png)`);
+            fullStackArr.push(brownArr.pop(brownArr[i]));
         }
     }
 
 
     if (Number(green2.textContent) > 0) {
         for (let i = 0; i < Number(green2.textContent); i++) {
-            fullStackArr.push(`url(./assets/MythicCards/green/green${greenArr.pop(greenArr[i])}.png)`);
+            fullStackArr.push(greenArr.pop(greenArr[i]));
         }
     }
 
     if (Number(blue1.textContent) > 0) {
         for (let i = 0; i < Number(blue1.textContent); i++) {
-            fullStackArr.push(`url(./assets/MythicCards/blue/blue${blueArr.pop(blueArr[i])}.png)`);
+            fullStackArr.push(blueArr.pop(blueArr[i]));
         }
     }
 
     if (Number(brown1.textContent) > 0) {
         for (let i = 0; i < Number(brown1.textContent); i++) {
-            fullStackArr.push(`url(./assets/MythicCards/brown/brown${brownArr.pop(brownArr[i])}.png)`);
+            fullStackArr.push(brownArr.pop(brownArr[i]));
         }
     }
 
 
     if (Number(green1.textContent) > 0) {
         for (let i = 0; i < Number(green1.textContent); i++) {
-            fullStackArr.push(`url(./assets/MythicCards/green/green${greenArr.pop(greenArr[i])}.png)`);
+            fullStackArr.push(greenArr.pop(greenArr[i]));
         }
     }
+
     fullStackArr.reverse();
 
+    let randomCol1 = fullStackArr.slice(0, stageSum1);
+    let randomCol2 = fullStackArr.slice(stageSum1, (stageSum1 + stageSum2));
+    let randomCol3 = fullStackArr.slice((stageSum1 + stageSum2), (stageSum1 + stageSum2 + stageSum3));
+
+    randomCol1.sort(() => Math.random() - 0.5);    
+    randomCol2.sort(() => Math.random() - 0.5); 
+    randomCol3.sort(() => Math.random() - 0.5); 
+
+    fullStackArr = randomCol1.concat(randomCol2,randomCol3);
 }
 
 export function diffLogic(diff) {
-    let activeDiff = String(diff.className).split('diff-').join('').split(' active').join('');
-
-    if (activeDiff === 2) {
-
-    }
+    activeDiff = Number(String(diff.className).split('diff-').join('').split(' active').join(''));
+    randomStack();
+    fullStack();
 }
